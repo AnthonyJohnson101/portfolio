e73ec5337f820e4cf2b30db44f8f357d632982bd
AnthonyJohnson101 <tonybologna123@outlook.com>
2024-12-24 15:03:07 -0500

    styling changes and particlejs implementation

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import About from './pages/About'
 import Skills from './pages/Skills'
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import ParticleBg from './components/particles'
 
 function App() {
   return (
     <main className='main-background'>
+      <ParticleBg id='particles' />
       <Home />
       <About />
       <Skills />
       <Projects />
       <Contact />
+      
     </main>
   );
 }
