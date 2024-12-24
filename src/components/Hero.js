da452eae6f4c1031a9dcce0cd3b5adc023fd5afe
AnthonyJohnson101 <tonybologna123@outlook.com>
2024-12-24 16:05:35 -0500

    small text changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,13 +12,13 @@ function Hero() {
                             <h4 className="mb-3">Hello! I am</h4>
                             <h4 className="mb-3">Tony Johnson</h4>
                             <h4 className="mb-3">Full Stack Web Developer</h4>
-                            <h4 className="mb-3">
+                            <p className="mb-3 fs-5">
                                 I’m a passionate Web Developer based in Michigan with experience in customer care, manufacturing, and retail.
                                 As a recent graduate of Michigan State University's Full Stack Web Development Bootcamp, I’m excited to transition into a tech-driven career and apply my skills in coding,
                                 problem-solving, and collaboration to develop impactful software solutions.
                                 <br /><br />
                                 I hold a Full Stack Web Development Certification from Michigan State University and have strong skills in front-end and back-end development, including HTML, CSS, JavaScript, React, Express.js, Node.js, and MongoDB.
-                            </h4>
+                            </p>
                             <a href={Resume} target="_blank" rel="noopener noreferrer" className="btn btn-outline-light m-1" download>Get my resume</a>
                         </div>
                     </div>
