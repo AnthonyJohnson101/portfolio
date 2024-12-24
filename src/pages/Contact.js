16c2c6616d24282facbda744ff149042009d767c
AnthonyJohnson101 <tonybologna123@outlook.com>
2024-12-24 14:30:36 -0500

    contact section update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContactCards from '../components/ContactCards';
+import Resume from '../assets/JohnsonResume.pdf'
 
 function Contact() {
     return (
@@ -7,12 +8,10 @@ function Contact() {
         <h1 className='row text-white mb-5'>Want To...</h1>
         <ContactCards />
         <div className='row text-white mb-5 d-flex flex-wrap py-5'>
-            <h2 className='col p-3'>Email</h2>
-            <h2 className='col p-3'>Email</h2>
-            <h2 className='col p-3'>Email</h2>
-            <h2 className='col p-3'>Email</h2>
-            <h2 className='col p-3'>Email</h2>
-            
+            <h2 className='col p-3'><a href="mailto:tonybologna123@outlook.com" className="text-white">Email</a></h2>
+            <h2 className='col p-3'><a href="https://www.linkedin.com/in/anthony-johnson-3602a41b2/" className="text-white">LinkedIn</a></h2>
+            <h2 className='col p-3'><a href="https://github.com/AnthonyJohnson101" className="text-white">Github</a></h2>
+            <h2 className='col p-3'><a href={Resume} className="text-white" download>Resume</a></h2>  
         </div>
     </div>
     );
