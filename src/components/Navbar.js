2b0ad635cb4e79a87dca32e03576d6be1940eb36
AnthonyJohnson101 <tonybologna123@outlook.com>
2024-12-25 20:05:01 -0500

    navbar adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,12 +14,12 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid px-5">
-        <a className="navbar-brand text-white" href="#home">Tony Johnson</a>
+        <a className="navbar-brand text-white fs-4" href="#home">Tony Johnson</a>
         <button className="navbar-toggler" type="button" onClick={toggleNavbar} aria-controls="navbarSupportedContent" aria-expanded={isOpen} aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <svg width="32px" height="32px" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" stroke=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2 12.32H22" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M2 18.32H22" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M2 6.32001H22" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
         </button>
         <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fs-5">
             <li className="nav-item">
               <a className="nav-link text-white" href="#about" onClick={closeNavbar}>About</a>
             </li>
